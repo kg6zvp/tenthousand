769e041e2ff53cca277556a83e4cdc2c793cef8b
stefanandonian <stefanandonian@Stefans-MacBook-Pro.local>
2015-10-19 13:28:02 -0700

    API server
    We are using AJAX calls to run some server code in our test website.
    Local host for the filesystem runs on port 8080. Local host for the api
    server runs on port 8081.

--- api/my_api_server.js
+++ api/my_api_server.js
@@ -1,10 +1,13 @@
 var http = require('http');
 
 http.createServer(function (req, res) {
+
+	var dictionary = require('url').parse(req.url, true);
+	var callback = dictionary.query.callback;
 	/** if statement about url name */
   res.writeHead(200, {'Content-Type': 'text/plain'});
   /** send engine a dictionary of queries, etc. */ 
-  res.end('Hello World\n');
+  res.end(callback+'("Hello World")');
 }).listen(8081);
 
 console.log('Server running on port 8081.');
--- api/public/app.js
+++ api/public/app.js
@@ -1,10 +1,9 @@
 var main = function(){
     $("button").click(function(){
-        $.ajax({url: "localhost:8081", success: function(result){
+        $.ajax({url: "http://localhost:8081", dataType: "jsonp", success: function(result){
             $("#test").html(result);
         }});
     });
-<div id= "test"><h2>Success</h2></div>
 };
 
 $(document).ready(main);
